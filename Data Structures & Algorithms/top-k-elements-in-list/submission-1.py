17801e0c5487402a47c03d412767328c728127f5
Yayi98 <superchintu98@gmail.com>
2026-05-01 17:45:19 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freqmap = Counter(nums)
+        xx=tuple(sorted((freqmap[key],key) for key in freqmap)[::-1])
+        return [xx[i][1] for i in range(k)]
